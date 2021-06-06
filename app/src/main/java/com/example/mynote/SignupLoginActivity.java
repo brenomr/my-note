57bca38461bc450c95a0af783c10b2872243721f
Breno Rodrigues <brenocartoon@hotmail.com>
2021-06-06 12:07:09 -0300

    Adicionar scripts para login

---
+++ app/src/main/java/com/example/mynote/SignupLoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.mynote;
+
+import android.app.Activity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mynote.R;
+
+public class SignupLoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_login);
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+        loginButton.setOnClickListener((v) -> {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                ProcessLogin();
+        });
+    }
+
+    private void ProcessLogin() {
+    }
+}
