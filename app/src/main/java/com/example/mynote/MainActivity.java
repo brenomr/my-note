a26d507aa67327dc3a63fc6c8ff086b843c4e18e
Breno Rodrigues <brenocartoon@hotmail.com>
2021-06-07 00:17:25 -0300

    Criar ou atualizar scripts com regras para a main activity e login.

--- app/src/main/java/com/example/mynote/MainActivity.java
+++ app/src/main/java/com/example/mynote/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.mynote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NoteClickListener {
@@ -18,14 +28,38 @@ public class MainActivity extends AppCompatActivity implements NoteClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView noteList = findViewById(R.id.noteList);
-        noteModels.add(new NoteModel("1", "Informação de teste 1", "06 Junho 14:00"));
-        noteModels.add(new NoteModel("2", "Informação de teste 2", "06 Junho 14:05"));
-        noteModels.add(new NoteModel("3", "Informação de teste 3", "06 Junho 14:10"));
+        final RecyclerView noteList = findViewById(R.id.noteList);
 
-        NoteItemsRecyclerViews noteItemsRecyclerViews = new NoteItemsRecyclerViews(noteModels,MainActivity.this);
-        noteList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        noteList.setAdapter(noteItemsRecyclerViews);
+        FloatingActionButton create = findViewById(R.id.create);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateNoteActivity.class));
+            }
+        });
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+
+        databaseReference.child(StaticUtils.getUserEmail(MainActivity.this)).child("noteModelList").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()) {
+                    String id = dataSnapshot1.child("id").getValue(String.class);
+                    String note = dataSnapshot1.child("note_data").getValue(String.class);
+                    String create = dataSnapshot1.child("created_at").getValue(String.class);
+                    noteModels.add(new NoteModel(id, note, create));
+                }
+                    NoteItemsRecyclerViews noteItemsRecyclerViews = new NoteItemsRecyclerViews(noteModels, MainActivity.this);
+                    noteList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                    noteList.setAdapter(noteItemsRecyclerViews);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/mynote/SignupLoginActivity.java
+++ app/src/main/java/com/example/mynote/SignupLoginActivity.java
@@ -2,6 +2,7 @@ package com.example.mynote;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -24,26 +25,99 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mynote.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class SignupLoginActivity extends AppCompatActivity {
+
+    DatabaseReference userDatabase;
+    private ProgressBar loadingProgressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_login);
+        getSupportActionBar().hide();
+        userDatabase = FirebaseDatabase.getInstance().getReference("users");
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        loadingProgressBar = findViewById(R.id.loading);
+
+        checkAlreadyLoggedIn();
 
         loginButton.setOnClickListener((v) -> {
+            if(usernameEditText.getText().toString().isEmpty()){
+                usernameEditText.setError("Informe seu e-mail.");
+                usernameEditText.requestFocus();
+                return;
+            }
+            if(passwordEditText.getText().toString().isEmpty()){
+                passwordEditText.setError("Informe sua senha.");
+                passwordEditText.requestFocus();
+                return;
+            }
                 loadingProgressBar.setVisibility(View.VISIBLE);
-                ProcessLogin();
+                ProcessLogin(usernameEditText.getText().toString(), passwordEditText.getText().toString());
         });
     }
 
-    private void ProcessLogin() {
+    private void checkAlreadyLoggedIn() {
+        if(StaticUtils.getUserEmail(SignupLoginActivity.this) != null) {
+            startActivity(new Intent(SignupLoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void ProcessLogin(final String email, final String password) {
+        userDatabase.child(email).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    String username1 = dataSnapshot.child("email").getValue(String.class);
+                    String password1 = dataSnapshot.child("password").getValue(String.class);
+                    String created = dataSnapshot.child("created_at").getValue(String.class);
+                    if(password1.equalsIgnoreCase(password)) {
+                        loadingProgressBar.setVisibility(View.GONE);
+                        StaticUtils.StoreLoggedEmail(SignupLoginActivity.this, email);
+                        startActivity(new Intent(SignupLoginActivity.this, MainActivity.class));
+                        finish();
+                    }
+                    else {
+                        loadingProgressBar.setVisibility(View.GONE);
+                        Toast.makeText(SignupLoginActivity.this,"Email ou senha inválidos.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                else {
+                    CreateNewUser(email, password);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void CreateNewUser(String email, String password) {
+
+        String id = userDatabase.push().getKey();
+
+        UserModel userModel = new UserModel(id, email, password);
+
+        userDatabase.child(email).setValue(userModel);
+        StaticUtils.StoreLoggedEmail(SignupLoginActivity.this, email);
         startActivity(new Intent(SignupLoginActivity.this, MainActivity.class));
+        finish();
     }
 }
