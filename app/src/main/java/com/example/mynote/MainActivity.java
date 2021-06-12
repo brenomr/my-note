f0ef36a6b6c1a91161ebe1bdfa571389aa2a4b90
Breno Rodrigues <brenocartoon@hotmail.com>
2021-06-12 20:05:34 -0300

    Adicionar novo ícone, novo menu e opção de logout da aplicação

--- app/src/main/java/com/example/mynote/MainActivity.java
+++ app/src/main/java/com/example/mynote/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
@@ -17,12 +20,25 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NoteClickListener {
 
     List<NoteModel> noteModels = new ArrayList<>();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem logout) {
+        StaticUtils.logout(MainActivity.this);
+        Intent tela = new Intent(this, SignupLoginActivity.class);
+        startActivity(tela);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +56,9 @@ public class MainActivity extends AppCompatActivity implements NoteClickListener
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
 
-        databaseReference.child(StaticUtils.getUserEmail(MainActivity.this)).child("noteModelList").addValueEventListener(new ValueEventListener() {
+        databaseReference.child(StaticUtils.getUsername(MainActivity.this)).child("noteModelList").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
                 for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()) {
                     String id = dataSnapshot1.child("id").getValue(String.class);
                     String note = dataSnapshot1.child("note_data").getValue(String.class);
@@ -57,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements NoteClickListener
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(MainActivity.this,"Erro ao acessar o banco.", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -68,7 +83,6 @@ public class MainActivity extends AppCompatActivity implements NoteClickListener
         intent.putExtra(Constants.id, noteModel.getId());
         intent.putExtra(Constants.note_text, noteModel.getNote_data());
         intent.putExtra(Constants.create_time, noteModel.getCreated_at());
-
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mynote/StaticUtils.java
+++ app/src/main/java/com/example/mynote/StaticUtils.java
@@ -4,15 +4,22 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class StaticUtils {
-    public static void StoreLoggedEmail(Context context, String email) {
+    public static void storeLoggedUsername(Context context, String username) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("MyPref", 0);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("email", email);
+        editor.putString("username", username);
         editor.apply();
     }
 
-    public static String getUserEmail(Context context) {
+    public static String getUsername(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("MyPref", 0);
-        return sharedPreferences.getString("email", null);
+        return sharedPreferences.getString("username", null);
+    }
+
+    public static void logout(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("MyPref", 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", null);
+        editor.apply();
     }
 }
